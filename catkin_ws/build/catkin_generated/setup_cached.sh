ed218b664ee4f581c7c2d2c88cb326662714182c
stinkyElias <elias.kalland@outlook.com>
2023-04-20 16:24:32 +0200

    Set up catkin package

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/elias/Documents/Robotics/polimi-ros-project1/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib:/home/elias/Documents/Robotics/polimi-ros/catkin_ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/lib'
+export PATH='/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/noetic/bin:/home/elias/.local/bin:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/noetic/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/sbin:/home/elias/.local/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/elias/Documents/Robotics/polimi-ros-project1/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/elias/Documents/Robotics/polimi-ros-project1/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/elias/Documents/Robotics/polimi-ros-project1/catkin_ws/src:$ROS_PACKAGE_PATH"
