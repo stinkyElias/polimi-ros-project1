75e1e2ba087ed7d266f3ef34bb73c2ca295620a9
stinkyElias <elias.kalland@outlook.com>
2023-04-21 16:00:03 +0200

    Delete catkin_ws directory

--- catkin_ws/build/first_project/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stinky/Documents/polimi-ros-project1/catkin_ws/src/first_project
-# Build directory: /home/stinky/Documents/polimi-ros-project1/catkin_ws/build/first_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
