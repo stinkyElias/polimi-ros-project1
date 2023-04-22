3010a63d8fbf89da40919cfa7a63da8ce0ae26d4
stinkyElias <elias.kalland@outlook.com>
2023-04-22 15:39:04 +0200

    readded files without bagfiles

---
+++ catkin_sw/build/first_project/CMakeFiles/odometry_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/stinky/Documents/polimi-ros-project1/catkin_sw/devel/lib/first_project/odometry_node"
+  "/home/stinky/Documents/polimi-ros-project1/catkin_sw/devel/lib/first_project/odometry_node.pdb"
+  "CMakeFiles/odometry_node.dir/src/odometry_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odometry_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_sw/src/first_project/src/odometry_node.cpp
@@ -0,0 +1,48 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Quaternion.h"
+// #include "nav_msgs.h"
+
+#include <sstream>
+
+class OdometryNode{
+    public:
+
+    private:
+
+
+};
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle n;
+
+    	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+	ros::Rate loop_rate(10);
+
+	int count = 0;
+  
+  
+  	while (ros::ok()){
+	    
+	    	std_msgs::String msg;
+
+    		std::stringstream ss;
+    		ss << "hello world " << count;
+    		msg.data = ss.str();
+
+    		ROS_INFO("%s", msg.data.c_str());
+
+    		chatter_pub.publish(msg);
+
+    		ros::spinOnce();
+
+    		loop_rate.sleep();
+    		++count;
+  	}
+    
+    return 0;
+}
