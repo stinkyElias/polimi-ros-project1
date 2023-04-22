a737e8dc0f45c4fba1e9946241c72c6d655681ec
stinkyElias <elias.kalland@outlook.com>
2023-04-22 15:36:45 +0200

    deleted workspace

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stinky/Documents/polimi-ros-project1/catkin_ws/src
-# Build directory: /home/stinky/Documents/polimi-ros-project1/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("first_project")
