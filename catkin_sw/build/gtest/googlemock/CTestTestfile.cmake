820afe3480c44c3c3428acd2ff1caff9562f3e4c
stinkyElias <elias.kalland@outlook.com>
2023-05-01 15:43:03 +0200

    Delete catkin_sw/build directory

--- catkin_sw/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/stinky/Documents/polimi-ros-project1/catkin_sw/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
